293870e598f31933f3c334d2b610ea0693281899
fernandomqs <fernandopozomqs@gmail.com>
2024-03-15 10:56:24 -0300

    push ecr

--- ecr.tf
+++ ecr.tf
@@ -67,3 +67,12 @@ resource "aws_ecr_lifecycle_policy" "repository-lifecycle" {
  EOF
 }
 
+
+# Definição de um recurso de execução local para fazer o push da imagem
+resource "null_resource" "push_image_to_ecr" {
+  provisioner "local-exec" {
+    command = "aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com && docker build -t ${aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${aws_ecr_repository.repository.name}:latest . && docker push ${aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${aws_ecr_repository.repository.name}:latest"
+  }
+
+  depends_on = [aws_ecr_repository.repository]
+}
--- vars.tf
+++ vars.tf
@@ -36,3 +36,13 @@ variable "tags" {
     Ambiente = "Desenvolvimento"
   }
 }
+
+# Define a variável com o ID da conta da AWS
+variable "aws_account_id" {
+  default = data.aws_caller_identity.current.account_id
+}
+
+# Define a variável com a região da AWS
+variable "aws_region" {
+  default = "us-east-1"
+}
