8fa814200f96e3430caef50f2560975a3b980c43
fernandomqs <fernandopozomqs@gmail.com>
2024-03-16 00:18:03 -0300

    ajuste

--- vars.tf
+++ vars.tf
@@ -37,6 +37,11 @@ variable "tags" {
   }
 }
 
+# Define a variável com o ID da conta da AWS
+variable "aws_account_id" {
+  default = data.aws_caller_identity.current.account_id
+}
+
 # Define a variável com a região da AWS
 variable "aws_region" {
   default = "us-east-1"
