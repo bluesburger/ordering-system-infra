43592551f0f6ce0e2d592ac04e698373c1c8b22e
Marlon Silva <78940998+MarlonSilva21@users.noreply.github.com>
2024-03-18 20:14:28 -0300

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "repository" {
-  name                 = "odering-system-bbs3"
+  name                 = "odering-system-bb"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
