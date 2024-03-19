90f7195c5b353ca7502ffcb3d9805612730608e1
fernandomqs <fernandopozomqs@gmail.com>
2024-03-19 04:10:18 -0300

    rolback

--- task.tf
+++ task.tf
@@ -4,7 +4,7 @@ resource "aws_ecs_task_definition" "task" {
     {
       name      = "${var.projectName}"
       essential = true,
-      image     = "851725351817.dkr.ecr.us-east-1.amazonaws.com/ordering-system-prod:latest",
+      image     = "${aws_ecr_repository.repository_url}:latest",
       environment = [
         {
           name  = "SPRING_PROFILES_ACTIVE"
