cc8e5cf8997db7515fef6111fc40b88eac5da31b
Marlon Silva <josemarlondasilva48@gmail.com>
2024-03-23 00:25:28 -0300

    refactoring: ajustando criacao de infra ecs

--- alb.tf
+++ alb.tf
@@ -2,10 +2,9 @@ resource "aws_lb" "cluster-application-load-balancer" {
   name               = "ALB-${var.project_name}"
   internal           = false
   load_balancer_type = var.load_balancer_type
-  security_groups    = data.aws_security_group.existing-load-balancer-security-group.id
+  security_groups    = [data.aws_security_group.existing-load-balancer-security-group.id]
   subnets            = [data.aws_subnet.existing-subnet-private-1.id, data.aws_subnet.existing-subnet-private-2.id]
   idle_timeout       = 60
-
 }
 
 resource "aws_lb_listener" "alb-listener-redirect" {
