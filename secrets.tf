2e049e894e01833610151cbc30586a2f4781a16c
fernandomqs <fernandopozomqs@gmail.com>
2024-05-13 18:04:09 -0300

    adicção de recursos para rodar o payment

--- secrets.tf
+++ secrets.tf
@@ -5,13 +5,13 @@ data "aws_secretsmanager_secret" "existing_payment_secret" {
 }
 
 resource "aws_secretsmanager_secret" "payment_secret" {
-  count      = data.aws_secretsmanager_secret.existing_payment_secret ? 0 : 1
+  count      = length(data.aws_secretsmanager_secret.existing_payment_secret) == 0 ? 1 : 0
   name                   = "/secret/payment"
   recovery_window_in_days = 7  # Permite a exclusão do segredo após 7 dias
 }
 
 resource "aws_secretsmanager_secret_version" "payment_secret_version" {
-  count          = data.aws_secretsmanager_secret.existing_payment_secret ? 0 : 1
+  count      = length(data.aws_secretsmanager_secret.existing_payment_secret) == 0 ? 1 : 0
   secret_id      = aws_secretsmanager_secret.payment_secret[count.index].id  # Usar sintaxe de elemento para acessar a instância específica
   secret_string  = "{\"MERCADO_PAGO_ACCESS_TOKEN\":\"APP_USR-7148288794739918-122720-66b9ac918d470a3bf62df3f0e6030f15-1613065352\"}"
 }
