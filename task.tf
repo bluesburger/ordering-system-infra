6267fdb9bf21865a9ec3e649f4c90406e0d430f4
fernandomqs <fernandopozomqs@gmail.com>
2024-05-13 19:31:16 -0300

    run

--- task.tf
+++ task.tf
@@ -116,61 +116,61 @@ resource "aws_ecs_task_definition" "task" {
 }
 
 //task para order
-resource "aws_ecs_task_definition" "task" {
-  family = "TSK-${var.projectName}"
-  container_definitions = jsonencode([
-    {
-      name      = "${var.project_name_order}"
-      essential = true,
-      image     = "${aws_ecr_repository.repository.repository_url}:order",
-      environment = [
-        {
-          name  = "SPRING_PROFILES_ACTIVE"
-          value = "dev"
-        },
-        {
-          name  = "SPRING_DATASOURCE_URL"
-          value = "jdbc:mysql://${data.aws_db_instance.database.endpoint}/${var.projectName}"
-        },
-        {
-          name  = "SPRING_DATASOURCE_USERNAME"
-          value = "${var.rdsUser}"
-        },
-        {
-          name  = "SPRING_DATASOURCE_PASSWORD"
-          value = "${var.rdsPass}"
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = "${aws_cloudwatch_log_group.cloudwatch-log-group.name}"
-          awslogs-region        = "us-east-1"
-          awslogs-stream-prefix = "ecs"
-        }
-      }
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
-  network_mode = "awsvpc"
-
-  requires_compatibilities = ["FARGATE"]
-
-  # execution_role_arn       = "${data.aws_iam_role.ecs_task_execution_role.arn}"
-  # execution_role_arn = "arn:aws:iam::${var.AWSAccount}:role/ecsTaskExecutionRole"
-  # execution_role_arn = "arn:aws:iam::${var.AWSAccount}:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
-  execution_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/OrderingSystemServiceRoleForECS"
-
-  memory = "4096"
-  cpu    = "2048"
-
-  # depends_on = [
-  #   data.aws_iam_role.ecs_task_execution_role
-  # ]
-}
+#resource "aws_ecs_task_definition" "task" {
+#  family = "TSK-${var.projectName}"
+#  container_definitions = jsonencode([
+#    {
+#      name      = "${var.project_name_order}"
+#      essential = true,
+#      image     = "${aws_ecr_repository.repository.repository_url}:order",
+#      environment = [
+#        {
+#          name  = "SPRING_PROFILES_ACTIVE"
+#          value = "dev"
+#        },
+#        {
+#          name  = "SPRING_DATASOURCE_URL"
+#          value = "jdbc:mysql://${data.aws_db_instance.database.endpoint}/${var.projectName}"
+#        },
+#        {
+#          name  = "SPRING_DATASOURCE_USERNAME"
+#          value = "${var.rdsUser}"
+#        },
+#        {
+#          name  = "SPRING_DATASOURCE_PASSWORD"
+#          value = "${var.rdsPass}"
+#        }
+#      ]
+#      logConfiguration = {
+#        logDriver = "awslogs"
+#        options = {
+#          awslogs-group         = "${aws_cloudwatch_log_group.cloudwatch-log-group.name}"
+#          awslogs-region        = "us-east-1"
+#          awslogs-stream-prefix = "ecs"
+#        }
+#      }
+#      portMappings = [
+#        {
+#          containerPort = 8080
+#          hostPort      = 8080
+#          protocol      = "tcp"
+#        }
+#      ]
+#    }
+#  ])
+#  network_mode = "awsvpc"
+#
+#  requires_compatibilities = ["FARGATE"]
+#
+#  # execution_role_arn       = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+#  # execution_role_arn = "arn:aws:iam::${var.AWSAccount}:role/ecsTaskExecutionRole"
+#  # execution_role_arn = "arn:aws:iam::${var.AWSAccount}:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
+#  execution_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/OrderingSystemServiceRoleForECS"
+#
+#  memory = "4096"
+#  cpu    = "2048"
+#
+#  # depends_on = [
+#  #   data.aws_iam_role.ecs_task_execution_role
+#  # ]
+#}
