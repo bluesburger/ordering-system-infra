a8437f29dae59ce0df538384d5cae93e082293f8
Marlon Silva <josemarlondasilva48@gmail.com>
2024-05-14 20:17:24 -0300

    test: testando criacao na aws

--- ecs.tf
+++ ecs.tf
@@ -39,43 +39,43 @@ resource "aws_ecs_service" "service_payment" {
   depends_on = [aws_lb.alb]
 }
 
-resource "aws_ecs_service" "service_order" {
-  name            = "SVC-${var.project_name_order}"
-  cluster         = aws_ecs_cluster.cluster.arn
-  task_definition = aws_ecs_task_definition.task_order.arn
-
-  desired_count                      = 1
-  deployment_minimum_healthy_percent = 100
-  deployment_maximum_percent         = 200
-
-  network_configuration {
-    subnets          = ["${data.aws_subnet.existing_subnet1.id}", "${data.aws_subnet.existing_subnet2.id}", "${data.aws_subnet.existing_subnet3.id}", "${data.aws_subnet.existing_subnet4.id}"]
-    security_groups  = ["${data.terraform_remote_state.other_repo.outputs.public_subnet_sg_id}"]
-    assign_public_ip = true
-  }
-
-  health_check_grace_period_seconds = 240
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.tg.arn
-    container_name   = var.project_name_order
-    container_port   = 8080
-  }
-
-  capacity_provider_strategy {
-    base              = 1
-    capacity_provider = "FARGATE"
-    weight            = 1
-  }
-
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-
-  deployment_controller {
-    type = "ECS"
-  }
-
-  depends_on = [aws_lb.alb]
-}
+#resource "aws_ecs_service" "service_order" {
+#  name            = "SVC-${var.project_name_order}"
+#  cluster         = aws_ecs_cluster.cluster.arn
+#  task_definition = aws_ecs_task_definition.task_order.arn
+#
+#  desired_count                      = 1
+#  deployment_minimum_healthy_percent = 100
+#  deployment_maximum_percent         = 200
+#
+#  network_configuration {
+#    subnets          = ["${data.aws_subnet.existing_subnet1.id}", "${data.aws_subnet.existing_subnet2.id}", "${data.aws_subnet.existing_subnet3.id}", "${data.aws_subnet.existing_subnet4.id}"]
+#    security_groups  = ["${data.terraform_remote_state.other_repo.outputs.public_subnet_sg_id}"]
+#    assign_public_ip = true
+#  }
+#
+#  health_check_grace_period_seconds = 240
+#
+#  load_balancer {
+#    target_group_arn = aws_lb_target_group.tg.arn
+#    container_name   = var.project_name_order
+#    container_port   = 8080
+#  }
+#
+#  capacity_provider_strategy {
+#    base              = 1
+#    capacity_provider = "FARGATE"
+#    weight            = 1
+#  }
+#
+#  deployment_circuit_breaker {
+#    enable   = true
+#    rollback = true
+#  }
+#
+#  deployment_controller {
+#    type = "ECS"
+#  }
+#
+#  depends_on = [aws_lb.alb]
+#}
