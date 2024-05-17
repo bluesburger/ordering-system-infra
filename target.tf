9e9e725e3cbd830c3ad6b1f650b3c37a9f25aeb8
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-05-16 21:37:46 -0300

    Correções para fazer funcionar

--- target.tf
+++ target.tf
@@ -7,7 +7,7 @@ resource "aws_lb_target_group" "tg" {
   vpc_id = data.aws_vpc.existing_vpcs.id
 
   health_check {
-    path    = "/swagger-ui/"
+    path    = "/actuator/health"
     port    = 8080
     matcher = "200,301"
   }
