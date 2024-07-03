75b9bda0d5340b1ce82d43d20ca0b116bf9e3d45
fernandomqs <fernandopozomqs@gmail.com>
2024-07-03 10:40:26 -0300

    ajuste

--- secrets.tf
+++ secrets.tf
@@ -1,24 +1,24 @@
-resource "aws_secretsmanager_secret" "payment_secret" {
-  name                   = "/secret/payment"
-  recovery_window_in_days = 7  # Permite a exclusão do segredo após 7 dias
-
-  # Ignorar alterações para evitar atualizações indesejadas
-  lifecycle {
-    ignore_changes = [
-      description,      # Ignorar mudanças na descrição do segredo
-      tags,             # Ignorar mudanças nas tags do segredo
-    ]
-  }
-}
-
-resource "aws_secretsmanager_secret_version" "payment_secret_version" {
-  # Dependência explícita do recurso de segredo para garantir a criação sequencial
-  depends_on = [aws_secretsmanager_secret.payment_secret]
-
-  secret_id     = aws_secretsmanager_secret.payment_secret.id
-  secret_string = jsonencode({
-    MERCADO_PAGO_ACCESS_TOKEN = "APP_USR-7148288794739918-122720-66b9ac918d470a3bf62df3f0e6030f15-1613065352"
-  })
-}
+# resource "aws_secretsmanager_secret" "payment_secret" {
+#   name                   = "/secret/payment"
+#   recovery_window_in_days = 7  # Permite a exclusão do segredo após 7 dias
+#
+#   # Ignorar alterações para evitar atualizações indesejadas
+#   lifecycle {
+#     ignore_changes = [
+#       description,      # Ignorar mudanças na descrição do segredo
+#       tags,             # Ignorar mudanças nas tags do segredo
+#     ]
+#   }
+# }
+#
+# resource "aws_secretsmanager_secret_version" "payment_secret_version" {
+#   # Dependência explícita do recurso de segredo para garantir a criação sequencial
+#   depends_on = [aws_secretsmanager_secret.payment_secret]
+#
+#   secret_id     = aws_secretsmanager_secret.payment_secret.id
+#   secret_string = jsonencode({
+#     MERCADO_PAGO_ACCESS_TOKEN = "APP_USR-7148288794739918-122720-66b9ac918d470a3bf62df3f0e6030f15-1613065352"
+#   })
+# }
 
 
